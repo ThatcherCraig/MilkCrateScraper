9d90ed6fb1cf5e6e61c142ae5db88816d710e6b4
thqtcher <thcraig@davidson.edu>
2021-04-29 18:17:40 -0400

    bug fix
    bug fix

--- finaldata.py
+++ finaldata.py
@@ -21,21 +21,6 @@ for line in Lines:
     closedata = line.strip().split()
     closeprice.append(closedata[0])
 input2.close()
-output = open("finaldata.txt","a+")
-date = str(datetime.datetime.now())
-output.write("Taken from wallstreetbets subreddit Daily Discussion Thread on " + date[0:10] + "\n" + "\n")
-netchange = []
-for i in range(len(toptickers)):
-    if(i == 5):
-        break
-    else:
-        output.write(str(toptickers[i]) + " - " + str(tickermentions[i]) + " mentions in " + str(comments) + " comments." + "\n")
-        totalchange = float(closeprice[i]) - float(openprice[i])
-        netchange.append(totalchange)
-        output.write(str(toptickers[i]) + "s price data on market open: $" + str(openprice[i]) + " and $" + str(closeprice[i]) + " on market close. Total Change: " + str(round(totalchange,2)) + "\n\n")
-output.write("Total increase in top mentioned stocks: $" + str(round(sum(netchange),2)) + "\n")
-output.write("------------------------------------------------------------------------------------------" + "\n")
-output.close()
 if(round(sum(netchange),2) >= 0):
     redgreen1 = "green"
 else:
@@ -88,7 +73,7 @@ for line in Lines:
 netchangedata.close()
 
 plt.figure(1)
-plt.title('Total # of Daily Comments')
+plt.title('Total # of Daily Comments over time')
 plt.ylabel('# of Comments')
 plt.xlabel('Date')
 plt.bar(datedata,commentdata,width=1,edgecolor = "black",color=redgreen3)
@@ -206,6 +191,21 @@ stocks will be recorded and graphed daily.</text>
 </html>"""
 f.write(html)
 f.close()
+output = open("finaldata.txt","a+")
+date = str(datetime.datetime.now())
+output.write("Taken from wallstreetbets subreddit Daily Discussion Thread on " + date[0:10] + "\n" + "\n")
+netchange = []
+for i in range(len(toptickers)):
+    if(i == 5):
+        break
+    else:
+        output.write(str(toptickers[i]) + " - " + str(tickermentions[i]) + " mentions in " + str(comments) + " comments." + "\n")
+        totalchange = float(closeprice[i]) - float(openprice[i])
+        netchange.append(totalchange)
+        output.write(str(toptickers[i]) + "s price data on market open: $" + str(openprice[i]) + " and $" + str(closeprice[i]) + " on market close. Total Change: " + str(round(totalchange,2)) + "\n\n")
+output.write("Total increase in top mentioned stocks: $" + str(round(sum(netchange),2)) + "\n")
+output.write("------------------------------------------------------------------------------------------" + "\n")
+output.close()
 output = open("finaldata.txt","a+")
 date = str(datetime.datetime.now())
 output.write("Taken from wallstreetbets subreddit Daily Discussion Thread on " + date[0:10] + "\n" + "\n")
