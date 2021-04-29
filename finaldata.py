20657d6ddf199a0dbe23b0d653dcfe7ee73d5ac4
thqtcher <thcraig@davidson.edu>
2021-04-29 18:27:43 -0400

    bug fix continued
    bug fix continued

--- finaldata.py
+++ finaldata.py
@@ -21,6 +21,14 @@ for line in Lines:
     closedata = line.strip().split()
     closeprice.append(closedata[0])
 input2.close()
+date = str(datetime.datetime.now())
+netchange = []
+for i in range(len(toptickers)):
+    if(i == 5):
+        break
+    else:
+        totalchange = float(closeprice[i]) - float(openprice[i])
+        netchange.append(totalchange)
 if(round(sum(netchange),2) >= 0):
     redgreen1 = "green"
 else:
@@ -191,21 +199,6 @@ stocks will be recorded and graphed daily.</text>
 </html>"""
 f.write(html)
 f.close()
-output = open("finaldata.txt","a+")
-date = str(datetime.datetime.now())
-output.write("Taken from wallstreetbets subreddit Daily Discussion Thread on " + date[0:10] + "\n" + "\n")
-netchange = []
-for i in range(len(toptickers)):
-    if(i == 5):
-        break
-    else:
-        output.write(str(toptickers[i]) + " - " + str(tickermentions[i]) + " mentions in " + str(comments) + " comments." + "\n")
-        totalchange = float(closeprice[i]) - float(openprice[i])
-        netchange.append(totalchange)
-        output.write(str(toptickers[i]) + "s price data on market open: $" + str(openprice[i]) + " and $" + str(closeprice[i]) + " on market close. Total Change: " + str(round(totalchange,2)) + "\n\n")
-output.write("Total increase in top mentioned stocks: $" + str(round(sum(netchange),2)) + "\n")
-output.write("------------------------------------------------------------------------------------------" + "\n")
-output.close()
 output = open("finaldata.txt","a+")
 date = str(datetime.datetime.now())
 output.write("Taken from wallstreetbets subreddit Daily Discussion Thread on " + date[0:10] + "\n" + "\n")
